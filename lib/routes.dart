b6f6aa37c11b524d3fe7a30fc82f13151438056c
raja-012 <kridraja01@gmail.com>
2025-08-04 18:37:19 +0200

    add Sign up  screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:monkey_meal_project/src/screens/sign_in/log_in_screen.dart';
+import 'package:monkey_meal_project/src/screens/sign_up/sign_up_screen.dart';
 import 'package:monkey_meal_project/src/screens/splash/landing_screen.dart';
 import 'package:monkey_meal_project/src/screens/splash/splash_screen.dart';
 
@@ -9,6 +10,7 @@ final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => const SplashScreen(),
   LandingScreen.routeName: (context) => const LandingScreen(),
   LoginScreen.routeName: (context) => const LoginScreen(),
+  SignUpScreen.routeName: (context) => const SignUpScreen(),
   //ForgetPwScreen.routeName: (context) =>  ForgetPwScreen(),
 
 
